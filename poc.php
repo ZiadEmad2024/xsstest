e7cbbc3a1200157f862eb999f4af5fcaafd66e5b
ZiadEmad2024 <eziad7091@gmail.com>
2025-08-11 20:25:31 +0300

    Update poc.php

--- poc.php
+++ poc.php
@@ -1,5 +1,6 @@
 <?php
 // SAFE SIMULATION - DOES NOT EXECUTE REAL COMMANDS
+// my site : https://ziademad2024.github.io/xsstest/poc.php
 if (isset($_GET['cmd'])) {
     $cmd = htmlspecialchars($_GET['cmd']); // Prevent execution
     echo "Simulated command execution: " . $cmd;
